8d46ea7fd4dc189bc7a8884181260dca2823ace3
Alexander Rössler <mail@roessler.systems>
2019-12-30 20:42:03 +0000

    ezgripper: code cleanup and add support for status publishing

--- ezgripper_driver/nodes/ezgripper.py
+++ ezgripper_driver/nodes/ezgripper.py
@@ -5,7 +5,7 @@
 #
 # Copyright (c) 2015, SAKE Robotics
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
@@ -31,8 +31,8 @@
 # POSSIBILITY OF SUCH DAMAGE.
 ##
 
-#  
-#  If you want to modify this program, you can find the "Main program" and 
+#
+#  If you want to modify this program, you can find the "Main program" and
 #  "Main loop" by searching for these terms.  They exist near the end of this file.
 #
 
@@ -42,121 +42,195 @@ from libezgripper import create_connection, Gripper
 import actionlib
 from control_msgs.msg import GripperCommandAction, GripperCommandResult
 from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
-from functools import partial
+from sensor_msgs.msg import JointState
+
+DIAG_UPDATE_INTERVAL_S = 1.0
+STATUS_UPDATE_INTERVAL_S = 0.2
 
 
-def calibrate_srv(gripper, msg):
-    rospy.loginfo("Calibrate service: request received")
-    gripper.calibrate()
-    gripper.open()
-    rospy.loginfo("Calibrate service: request completed")
-    return EmptyResponse()
-
-class GripperActionServer:
+class GripperActionServer(object):
     def __init__(self, action_name, gripper):
         self.gripper = gripper
-        self.action_server = actionlib.SimpleActionServer(action_name, GripperCommandAction, self.gripper_action_execute, False)
+        self.action_server = actionlib.SimpleActionServer(
+            action_name,
+            GripperCommandAction,
+            self.gripper_action_execute,
+            False,
+        )
         self.action_server.start()
-        
+
     def gripper_action_execute(self, goal):
-        rospy.loginfo("Execute goal: position=%.1f, max_effort=%.1f"%
-                      (goal.command.position, goal.command.max_effort))
-        
+        rospy.loginfo(
+            "Execute goal: position=%.1f, max_effort=%.1f"
+            % (goal.command.position, goal.command.max_effort)
+        )
+
         if goal.command.max_effort == 0.0:
             rospy.loginfo("Release torque: start")
             self.gripper.release()
             rospy.loginfo("Release torque: done")
         else:
             rospy.loginfo("Go to position: start")
-            self.gripper.goto_position(goal.command.position, goal.command.max_effort)
+            self.gripper.goto_position(
+                goal.command.position, goal.command.max_effort
+            )
             rospy.loginfo("Go to position: done")
 
         result = GripperCommandResult()
-        result.position = goal.command.position #not necessarily the current position of the gripper if the gripper did not reach its goal position.
+        # not necessarily the current position of the gripper if the gripper did not reach its goal position.
+        result.position = goal.command.position
         result.effort = goal.command.max_effort
         result.stalled = False
         result.reached_goal = True
         self.action_server.set_succeeded(result)
-    
-def send_diags():
-    # See diagnostics with: rosrun rqt_runtime_monitor rqt_runtime_monitor
-    msg = DiagnosticArray()
-    msg.status = []
-    msg.header.stamp = rospy.Time.now()
-    
-    for gripper in grippers:
-        for servo in gripper.servos:
-            status = DiagnosticStatus()
-            status.name = "Gripper '%s' servo %d"%(gripper.name, servo.servo_id)
-            status.hardware_id = '%s'%servo.servo_id
-            temperature = servo.read_temperature()
-            status.values.append(KeyValue('Temperature', str(temperature)))
-            status.values.append(KeyValue('Voltage', str(servo.read_voltage())))
-    
-            if temperature >= 70:
-                status.level = DiagnosticStatus.ERROR
-                status.message = 'OVERHEATING'
-            elif temperature >= 65:
-                status.level = DiagnosticStatus.WARN
-                status.message = 'HOT'
-            else:
-                status.level = DiagnosticStatus.OK
-                status.message = 'OK'
-        
-            msg.status.append(status)
-    
-    diagnostics_pub.publish(msg)
 
 
-# Main Program
+class GripperDiagnostics(object):
+    def __init__(self, grippers):
+        self._grippers = grippers
 
-rospy.init_node('ezgripper')
-rospy.loginfo("Started")
+        self._pub = rospy.Publisher(
+            '/diagnostics', DiagnosticArray, queue_size=1
+        )
 
-port_name = rospy.get_param('~port', '/dev/ttyUSB0')
-baud = int(rospy.get_param('~baud', '57600'))
-gripper_params = rospy.get_param('~grippers')
+    def send_diags(self):
+        # See diagnostics with: rosrun rqt_runtime_monitor rqt_runtime_monitor
+        msg = DiagnosticArray()
+        msg.status = []
+        msg.header.stamp = rospy.Time.now()
 
-diagnostics_pub = rospy.Publisher('/diagnostics', DiagnosticArray, queue_size=1)
+        for gripper in (g.gripper for g in self._grippers):
+            for servo in gripper.servos:
+                status = DiagnosticStatus()
+                status.name = "Gripper '%s' servo %d" % (
+                    gripper.name,
+                    servo.servo_id,
+                )
+                status.hardware_id = '%s' % servo.servo_id
+                temperature = servo.read_temperature()
+                status.values.append(KeyValue('Temperature', str(temperature)))
+                status.values.append(
+                    KeyValue('Voltage', str(servo.read_voltage()))
+                )
 
-connection = create_connection(port_name, baud)
+                if temperature >= 70:
+                    status.level = DiagnosticStatus.ERROR
+                    status.message = 'OVERHEATING'
+                elif temperature >= 65:
+                    status.level = DiagnosticStatus.WARN
+                    status.message = 'HOT'
+                else:
+                    status.level = DiagnosticStatus.OK
+                    status.message = 'OK'
 
-all_servos = []
-references = []
-grippers = []
+                msg.status.append(status)
 
-for gripper_name, servo_ids in gripper_params.iteritems():
-    gripper = Gripper(connection, gripper_name, servo_ids)
-    all_servos += gripper.servos
+        self._pub.publish(msg)
 
-    gripper.calibrate()
-    gripper.open()
-    
-    references.append( rospy.Service('~'+gripper_name+'/calibrate', Empty, partial(calibrate_srv, gripper)) )
-    references.append( GripperActionServer('~'+gripper_name, gripper) )
-    
-    grippers.append(gripper)
 
-# Main Loop
+class GripperStatus(object):
+    PALM_L1_CLOSED_POS = 1.84
+    L1_L2_CLOSED_POS = 0.0
 
-r = rospy.Rate(20) # hz
-diags_last_sent = 0
-while not rospy.is_shutdown():
-    now = rospy.get_time()
-    if now - diags_last_sent > 1.0:
-        try:
-            send_diags()
-            diags_last_sent = now
-        except Exception, e:
-            rospy.logerr("Exception while reading diagnostics: %s"%e)
-            
-    for servo in all_servos:
-        try:
-            servo.check_overload_and_recover()
-        except Exception, e:
-            rospy.logerr("Exception while checking overload: %s"%e)
-            servo.flushAll()
+    def __init__(self, grippers):
+        self._grippers = grippers
 
-    r.sleep()
-    
-rospy.loginfo("Exiting")
+        self._pub = rospy.Publisher('joint_states', JointState, queue_size=5)
+
+    def publish_status(self):
+        msg = JointState()
+        msg.header.stamp = rospy.Time.now()
+        for gripper in (g.gripper for g in self._grippers):
+            # gripper.name
+            pos = gripper.get_position()
+            joint_pos = (100.0 - pos) / 100.0 * self.PALM_L1_CLOSED_POS
+            msg.name.append(
+                '{}_ezgripper_knuckle_palm_L1_1'.format(gripper.name)
+            )
+            msg.position.append(joint_pos)
+            joint_pos = (100.0 - pos) / 100.0 * self.L1_L2_CLOSED_POS
+            msg.name.append(
+                'main_ezgripper_knuckle_L1_L2_1'.format(gripper.name)
+            )
+            msg.position.append(joint_pos)
+
+        self._pub.publish(msg)
+
+
+class EZGripper(object):
+    def __init__(self, connection, gripper_name, servo_ids):
+        self.gripper = Gripper(connection, gripper_name, servo_ids)
+
+        self._action_srv = GripperActionServer('~' + gripper_name, self.gripper)
+        self._calibrate_srv = rospy.Service(
+            '~' + gripper_name + '/calibrate', Empty, self._calibrate_srv
+        )
+
+        self.gripper.calibrate()
+        self.gripper.open()
+
+    def _calibrate_srv(self, _msg):
+        rospy.loginfo("Calibrate service: request received")
+        self.gripper.calibrate()
+        self.gripper.open()
+        rospy.loginfo("Calibrate service: request completed")
+        return EmptyResponse()
+
+
+def main():
+    # Main Program
+
+    rospy.init_node('ezgripper')
+    rospy.loginfo("Started")
+
+    port_name = rospy.get_param('~port', '/dev/ttyUSB0')
+    baud = int(rospy.get_param('~baud', '57600'))
+    gripper_params = rospy.get_param('~grippers')
+
+    connection = create_connection(port_name, baud)
+
+    all_servos = []
+    grippers = []
+
+    for gripper_name, servo_ids in gripper_params.items():
+        gripper = EZGripper(connection, gripper_name, servo_ids)
+        all_servos += gripper.gripper.servos
+        grippers.append(gripper)
+
+    diagnostics = GripperDiagnostics(grippers)
+    status = GripperStatus(grippers)
+
+    # Main Loop
+    r = rospy.Rate(20)  # hz
+    diags_last_sent = 0
+    status_last_sent = 0
+    while not rospy.is_shutdown():
+        now = rospy.get_time()
+        if now - diags_last_sent > DIAG_UPDATE_INTERVAL_S:
+            try:
+                diagnostics.send_diags()
+                diags_last_sent = now
+            except Exception as e:
+                rospy.logerr("Exception while reading diagnostics: %s" % e)
+
+        if now - status_last_sent > STATUS_UPDATE_INTERVAL_S:
+            try:
+                status.publish_status()
+                status_last_sent = now
+            except Exception as e:
+                rospy.logerr("Exception while publishing status %s" % e)
+
+        for servo in all_servos:
+            try:
+                servo.check_overload_and_recover()
+            except Exception as e:
+                rospy.logerr("Exception while checking overload: %s" % e)
+                servo.flushAll()
+
+        r.sleep()
+
+    rospy.loginfo("Exiting")
+
+
+if __name__ == '__main__':
+    main()
--- ezgripper_driver/src/libezgripper/ezgripper_base.py
+++ ezgripper_driver/src/libezgripper/ezgripper_base.py
@@ -5,7 +5,7 @@
 #
 # Copyright (c) 2016, SAKE Robotics
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
@@ -31,6 +31,7 @@
 # POSSIBILITY OF SUCH DAMAGE.
 ##
 
+from __future__ import print_function
 from lib_robotis import create_connection, Robotis_Servo
 import time
 
@@ -41,79 +42,103 @@ def set_torque_mode(servo, val):
     else:
         servo.write_address(70, [0])
 
+
 def wait_for_stop(servo):
     wait_start = time.time()
-    last_position = 1000000 # read_encoder() cannot return more than 65536
+    last_position = 1000000  # read_encoder() cannot return more than 65536
     while True:
         current_position = servo.read_encoder()
         if current_position == last_position:
             break
         last_position = current_position
         time.sleep(0.1)
-        
+
         if time.time() - wait_start > 5:
             break
 
 
-class Gripper:
-    
-    GRIP_MAX = 2500 # maximum open position for grippers
-    TORQUE_MAX = 800 # maximum torque - MX-64=500, MX-106=350
-    TORQUE_HOLD = 13 # This is percentage of TORQUE_MAX. In absolute units: holding torque - MX-64=100, MX-106=80
-   
+class Gripper(object):
+    GRIP_MAX = 2500  # maximum open position for grippers
+    TORQUE_MAX = 800  # maximum torque - MX-64=500, MX-106=350
+    TORQUE_HOLD = (
+        13
+    )  # This is percentage of TORQUE_MAX. In absolute units: holding torque - MX-64=100, MX-106=80
+
     def __init__(self, connection, name, servo_ids):
         self.name = name
-        self.servos = [Robotis_Servo( connection, servo_id ) for servo_id in servo_ids]
+        self.servos = [
+            Robotis_Servo(connection, servo_id) for servo_id in servo_ids
+        ]
         for servo in self.servos:
-            servo.ensure_byte_set(22, 1) # Make sure 'Resolution divider' is set to 1
+            servo.ensure_byte_set(
+                22, 1
+            )  # Make sure 'Resolution divider' is set to 1
         self.zero_positions = [0] * len(self.servos)
-    
+
     def scale(self, n, to_max):
         # Scale from 0..100 to 0..to_max
         result = int(n * to_max / 100)
-        if result > to_max: result = to_max
-        if result < 0: result = 0
+        if result > to_max:
+            result = to_max
+        if result < 0:
+            result = 0
         return result
-    
-    def down_scale (self, n, to_max):
+
+    def down_scale(self, n, to_max):
         # Scale from 0..to_max to 0..100
         result = int(round(n * 100.0 / to_max))
-        if result > 100: result = 100
-        if result < 0: result = 0
+        if result > 100:
+            result = 100
+        if result < 0:
+            result = 0
         return result
-        
+
     def calibrate(self):
-        print "calibrating: " + self.name
-        
+        print("calibrating: " + self.name)
+
         for servo in self.servos:
-            servo.write_address(6, [255,15,255,15] )   # 1) "Multi-Turn" - ON
-            servo.write_word(34, 500)                  # 2) "Torque Limit" to 500 (or so)
-            servo.write_address(24, [0])               # 3) "Torque Enable" to OFF
-            servo.write_address(70, [1])               # 4) Set "Goal Torque Mode" to ON
-            servo.write_word(71, 1024 + 100)           # 5) Set "Goal Torque" Direction to CW and Value 100
-        
-        time.sleep(4.0)                               # 6) give it time to stop
-        
+            servo.write_address(6, [255, 15, 255, 15])  # 1) "Multi-Turn" - ON
+            servo.write_word(34, 500)  # 2) "Torque Limit" to 500 (or so)
+            servo.write_address(24, [0])  # 3) "Torque Enable" to OFF
+            servo.write_address(70, [1])  # 4) Set "Goal Torque Mode" to ON
+            servo.write_word(
+                71, 1024 + 100
+            )  # 5) Set "Goal Torque" Direction to CW and Value 100
+
+        time.sleep(4.0)  # 6) give it time to stop
+
         for i in range(len(self.servos)):
             servo = self.servos[i]
-            servo.write_word(71, 1024 + 10)            # 7) Set "Goal Torque" Direction to CW and Value 10 - reduce load on servo
-            servo.write_word(20, 0)                    # 8) set "Multi turn offset" to 0   
-            self.zero_positions[i] = servo.read_word_signed(36) # 9) read current position of servo
-        
-        print "calibration done"
-    
+            servo.write_word(
+                71, 1024 + 10
+            )  # 7) Set "Goal Torque" Direction to CW and Value 10 - reduce load on servo
+            servo.write_word(20, 0)  # 8) set "Multi turn offset" to 0
+            self.zero_positions[i] = servo.read_word_signed(
+                36
+            )  # 9) read current position of servo
+
+        print("calibration done")
+
     def set_max_effort(self, max_effort):
         # sets torque for moving to position (moving_torque) and for torque only mode (torque_mode_max_effort)
         # range 0-100% (0-100) - this range is in 0-100 whole numbers so that it can be used where Newton force is expected
         # max dynamixel torque is 0-1023(unitless)
 
-        torque_mode_max_effort = moving_torque = self.scale(max_effort, self.TORQUE_MAX)
+        torque_mode_max_effort = moving_torque = self.scale(
+            max_effort, self.TORQUE_MAX
+        )
 
-        print "set_max_effort(%d): moving torque: %d, goal torque: %d"%(
-                    max_effort, moving_torque, torque_mode_max_effort)
+        print(
+            "set_max_effort(%d): moving torque: %d, goal torque: %d"
+            % (max_effort, moving_torque, torque_mode_max_effort)
+        )
         for servo in self.servos:
-            servo.write_word(34, moving_torque) # torque for moving to position,and due to Dynamixel architecture, this also limits max value for register 71 below
-            servo.write_word(71, 1024+torque_mode_max_effort) # torque mode of closing gripper
+            servo.write_word(
+                34, moving_torque
+            )  # torque for moving to position,and due to Dynamixel architecture, this also limits max value for register 71 below
+            servo.write_word(
+                71, 1024 + torque_mode_max_effort
+            )  # torque mode of closing gripper
 
     def _goto_position(self, position):
         for servo in self.servos:
@@ -121,14 +146,17 @@ class Gripper:
         for i in range(len(self.servos)):
             self.servos[i].write_word(30, self.zero_positions[i] + position)
         wait_for_stop(self.servos[0])
-        
+
     def _close_with_torque(self):
         for servo in self.servos:
             set_torque_mode(servo, True)
         wait_for_stop(self.servos[0])
 
     def get_position(self, servo_num=0):
-        servo_position = self.servos[servo_num].read_word_signed(36) - self.zero_positions[servo_num]
+        servo_position = (
+            self.servos[servo_num].read_word_signed(36)
+            - self.zero_positions[servo_num]
+        )
         return self.down_scale(servo_position, self.GRIP_MAX)
 
     def get_positions(self):
@@ -141,26 +169,31 @@ class Gripper:
         # Using the 0-100% range allows the user to define the definition of where the gap is measured.
         # position: 0..100, 0 - close, 100 - open
         # closing_torque: 0..100
-        
+
         servo_position = self.scale(position, self.GRIP_MAX)
-        print "goto_position(%d, %d): servo position %d"%(position, closing_torque, servo_position)
-        self.set_max_effort(closing_torque)  # essentially sets velocity of movement, but also sets max_effort for initial half second of grasp.
+        print(
+            "goto_position(%d, %d): servo position %d"
+            % (position, closing_torque, servo_position)
+        )
+        self.set_max_effort(
+            closing_torque
+        )  # essentially sets velocity of movement, but also sets max_effort for initial half second of grasp.
 
         if position == 0:
             self._close_with_torque()
         else:
             self._goto_position(servo_position)
-        
+
         # Sets torque to keep gripper in position, but does not apply torque if there is no load.
         # This does not provide continuous grasping torque.
         holding_torque = min(self.TORQUE_HOLD, closing_torque)
         self.set_max_effort(holding_torque)
-        print "goto_position done"
+        print('goto_position done')
 
     def release(self):
         for servo in self.servos:
             set_torque_mode(servo, False)
-            
+
     def open(self):
         self.goto_position(100, 100)
 
@@ -170,26 +203,27 @@ class Gripper:
             temperatures.append(servo.read_temperature())
         return temperatures
 
+
 if __name__ == '__main__':
     # Sample code
     connection = create_connection(dev_name='/dev/ttyUSB0', baudrate=57600)
-    #connection = create_connection(dev_name='hwgrep://0403:6001', baudrate=57600)
-    #connection = create_connection(dev_name='socket://127.0.0.1:4000', baudrate=57600)
+    # connection = create_connection(dev_name='hwgrep://0403:6001', baudrate=57600)
+    # connection = create_connection(dev_name='socket://127.0.0.1:4000', baudrate=57600)
     gripper = Gripper(connection, 'gripper1', [1])
-    #gripper = Gripper(connection, 'gripper1', [1,2])
+    # gripper = Gripper(connection, 'gripper1', [1,2])
 
-    print "temperatures:", gripper.get_temperatures()
+    print("temperatures:", gripper.get_temperatures())
 
     gripper.calibrate()
-    gripper.goto_position(100, 100) # open
+    gripper.goto_position(100, 100)  # open
     time.sleep(2.0)
-    print "positions:", gripper.get_positions()
-    gripper.goto_position(0, 50) # close
+    print("positions:", gripper.get_positions())
+    gripper.goto_position(0, 50)  # close
     time.sleep(2.0)
-    print "positions:", gripper.get_positions()
-    gripper.goto_position(100, 50) # open
+    print("positions:", gripper.get_positions())
+    gripper.goto_position(100, 50)  # open
     time.sleep(2.0)
-    gripper.goto_position(70, 100) # position 70
-    print "positions:", gripper.get_positions()
-    print "position:", gripper.get_position()
-    print "DONE"
+    gripper.goto_position(70, 100)  # position 70
+    print("positions:", gripper.get_positions())
+    print("position:", gripper.get_position())
+    print("DONE")
